e0ce2ba97ff330fc23608d6f4c946c396149e81b
terence-meunier <terence.meunier@gmail.com>
2021-08-10 14:39:58 +0200

    Add Componenets

---
+++ components/html/inputText.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'inputText',
+    template: `<section class="py-5 flex flex-col justify-center items-center">
+    <label for="input-search">Entrer votre destination :</label>
+    <input
+      id="input-search"
+      type="text"
+      placeholder="Lyon, Marseille, etc."
+      class=" text-center mt-2 h-14 rounded-3xl w-5/6 text-10 bg-gray-50 px-10 border-blue-500 border-2"
+    >
+  </section>`
+}
